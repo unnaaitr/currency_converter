d90169b9d0edbd366b36f8b14c9364b3c1b24048
unnaaitr <unnaaitr@gmail.com>
2026-07-16 00:04:52 +0200

    Added tries and excepts

--- currency_converter.py
+++ currency_converter.py
@@ -15,31 +15,41 @@ rates = data["conversion_rates"]
 
 # We print the convertions
 print("\nRates to $1 USD:")
-print(f"- {rates['EUR']} €") # Euros
-print(f"- {rates['GBP']} £") # Pounds
-print(f"- {rates['JPY']} ¥\n") # Yens
+print(f"- {rates['EUR']:.3f} €") # Euros
+print(f"- {rates['GBP']:.3f} £") # Pounds
+print(f"- {rates['JPY']:.3f} ¥\n") # Yens
 
 
 # We ask the user what he wants, solving minor issues like money < 0
 while True:
-    amount = float(input("Choose the quantity to convert: "))
-    print(" ")
+    try:
+        amount = float(input("Choose the quantity to convert: "))
+        print(" ") # Added another line to make it look cooler
 
-    if amount <= 0:
-        print("Try again!\n")  
+        if amount <= 0:
+            print("Try again!\n")  
 
-    else:
-        from_currency = str(input("- Which currency is it? ")).strip().upper()
-        to_currency = str(input("- Which currency do you want to convert it to? ")).strip().upper()
-        break
+        else:  
+            from_currency = str(input("- Which currency is it? ")).strip().upper()
+            to_currency = str(input("- Which currency do you want to convert it to? ")).strip().upper()
+
+            if from_currency not in rates or to_currency not in rates:
+                print("Enter a valid currency!\n")
+
+            else:
+                break
+
+    except ValueError:
+        print("Enter a valid number!\n")   
 
 
 # We just need a simple function for it all
-def convert(amount, from_currency, to_currency, rates):
-    result = (amount / rates[from_currency]) * rates[to_currency]
-    print(f"{amount} {from_currency} = {result:.2f} {to_currency}\n")
-    return result
+def convert(amount, from_currency, to_currency, rates):    
+        result = (amount / rates[from_currency]) * rates[to_currency] # AI's formula
+        print(f"{amount} {from_currency} = {result:.2f} {to_currency}\n")
 
+        return result
+        
 
 # Then we call the function to let it work
 convert(amount, from_currency, to_currency, rates)
